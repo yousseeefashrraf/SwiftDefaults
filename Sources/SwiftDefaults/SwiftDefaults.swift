e2097dc858e94f136673505a375b8fb6c8719ae4
yousseeefashrraf <yousseeefashraf@gmail.com>
2025-07-28 00:54:38 +0300

    Public package.

--- Sources/SwiftDefaults/SwiftDefaults.swift
+++ Sources/SwiftDefaults/SwiftDefaults.swift
@@ -8,13 +8,13 @@ import Foundation
 /// Use this protocol when you want to manage default values and persistent storage
 /// in a standardized and type-safe way.
 
-protocol DefaultValue{
+public protocol DefaultValue{
     associatedtype defaultType
     static var defaultValue: defaultType {get}
     static var key: String {get}
 }
 
-final class UserDefaultsManager: @unchecked Sendable {
+final public class UserDefaultsManager: @unchecked Sendable {
     static let shared = UserDefaultsManager()
     private init () {}
     
@@ -24,7 +24,7 @@ final class UserDefaultsManager: @unchecked Sendable {
     /// If no value is found, it stores the type's `defaultValue` using its associated `key`.
     ///
     /// - Parameter type: The type conforming to `DefaultValue` that provides both a `defaultValue` and a `key`.
-    func initiate<T: DefaultValue>(forType type: T.Type) where T.defaultType: RawRepresentable, T.defaultType.RawValue == String{
+    public func initiate<T: DefaultValue>(forType type: T.Type) where T.defaultType: RawRepresentable, T.defaultType.RawValue == String{
         if !isStored(forKey: type.key) {
             UserDefaults.standard.setValue(type.defaultValue.rawValue, forKey: type.key)
         }
@@ -40,7 +40,7 @@ final class UserDefaultsManager: @unchecked Sendable {
     ///   - key: The key associated with the stored value in `UserDefaults`.
     /// - Returns: A Boolean value indicating whether a value exists for the given key.
     ///
-    func isStored(forKey key: String) -> Bool{
+    public func isStored(forKey key: String) -> Bool{
         UserDefaults.standard.object(forKey: key) != nil
     }
     
@@ -50,13 +50,13 @@ final class UserDefaultsManager: @unchecked Sendable {
     ///   - type: A type conforming to `DefaultValue` protocol, used to get the key.
     ///   - value: The value to store in `UserDefaults`.
     ///
-    func store<T: DefaultValue>(forType type: T.Type,value: T) where T: RawRepresentable, T.RawValue == String {
+    public func store<T: DefaultValue>(forType type: T.Type,value: T) where T: RawRepresentable, T.RawValue == String {
         if isStored(forKey: type.key) {
             UserDefaults.standard.setValue(value.rawValue, forKey: type.key)
         }
     }
     
-    func getStoredValue<T: DefaultValue>(forType type: T.Type)  -> String? where T.defaultType: RawRepresentable, T.defaultType.RawValue == String{
+    public func getStoredValue<T: DefaultValue>(forType type: T.Type)  -> String? where T.defaultType: RawRepresentable, T.defaultType.RawValue == String{
 
         let result = UserDefaults.standard.string(forKey: type.key)
             return result
